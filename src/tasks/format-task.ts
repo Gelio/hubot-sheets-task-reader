c5f3fa78dfd3230ca308e0fa7f517760a086bffd
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2020-03-21 18:20:33 +0100

    add special formatting when nobody is assigned to a task

--- src/tasks/format-task.ts
+++ src/tasks/format-task.ts
@@ -2,5 +2,9 @@ import { formatUser } from '../users/format-user';
 import { Task } from './types';
 
 export function formatTask(task: Task) {
+  if (task.assignees.length === 0) {
+    return `${task.name}: nobody`;
+  }
+
   return `${task.name}: ${task.assignees.map(formatUser).join(', ')}`;
 }
